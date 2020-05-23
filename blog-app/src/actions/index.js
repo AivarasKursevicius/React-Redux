f394205230d98cbaf23642165b95011405975de5
Aivaras <aivaraskursevicius@gmail.com>
2020-05-23 23:24:47 +0300

    finished blog-app

--- blog-app/src/actions/index.js
+++ blog-app/src/actions/index.js
@@ -1,4 +1,13 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  console.log("about to fetched posts");
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -11,3 +20,11 @@ export const fetchUser = (id) => async (dispatch) => {
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
